faa9b122158af1d0f5914fec1d5001b77051c58b
none <stonejing@users.noreply.github.com>
2022-05-15 18:27:29 +0900

    add basic function of conway life game

--- src/ConwayLifeGameCanvas/ConwayLifeGameCanvas.js
+++ src/ConwayLifeGameCanvas/ConwayLifeGameCanvas.js
@@ -1,127 +1,109 @@
 import React, {useState, useEffect, useRef} from "react";
-import { WindowToScreen, DeepCopy, InitiateBoard } from './utils';
+import { WindowToScreen, RandomBoard, GameofLife } from './utils';
 
 function ConwayLifeGameCanvas()
 {
     const canvasRef = useRef(null);
-    const [board, setBoard] = useState([]);
+    const boardRef = useRef(null);
+
+    const stepx = 5;
+    const stepy = 5;
 
     /**
      * 
      * @param {CanvasRenderingContext2D} context 
      */
     const drawGrid = (context, color, stepx, stepy) => {
-        context.strokeStyle = color;
-        context.lineWidth = 0.5;
-
-        for(let i = 0.5; i < context.canvas.width; i += stepx)
-        {
-            context.beginPath();
-            context.moveTo(i, 0);
-            context.lineTo(i, context.canvas.height);
-            context.stroke();
-        }
-        for(let i = 0.5; i < context.canvas.height; i += stepy)
-        {
-            context.beginPath();
-            context.moveTo(0, i);
-            context.lineTo(context.canvas.width, i);
-            context.stroke();
-        }
+      context.strokeStyle = color;
+      context.lineWidth = 0.5;
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      for(let i = 0.5; i < context.canvas.width; i += stepx)
+      {
+        context.beginPath();
+        context.moveTo(i, 0);
+        context.lineTo(i, context.canvas.height);
+        context.stroke();
+      }
+      for(let i = 0.5; i < context.canvas.height; i += stepy)
+      {
+        context.beginPath();
+        context.moveTo(0, i);
+        context.lineTo(context.canvas.width, i);
+        context.stroke();
+      }
     }
 
-    const drawBoard = (context, board) => {
-        for(let i = 0; i < board.length; i++)
-        {
-            for(let j = 0; j < board[0].length; j++)
-            {
-                if(board[i][j] === 1)
-                {
-                    let x = 10 * i;
-                    let y = 10 * j;
-                    context.fillRect(x, y, 10, 10);
-                }
-            }
-        }
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} context 
+     * @param {*} boardRef 
+     * @param {*} stepx 
+     * @param {*} stepy 
+     */
+    const drawBoard = (context, boardRef, stepx, stepy) => {
+      for(let i = 0; i < boardRef.current.length; i++)
+      {
+          for(let j = 0; j < boardRef.current[0].length; j++)
+          {
+              if(boardRef.current[i][j] === 1)
+              {
+                  let x = stepx * i;
+                  let y = stepy * j;
+                  context.fillRect(x, y, stepx, stepy);
+              }
+          }
+      }
     }
 
     const onMouseClick = (e) => {
-        e.preventDefault();
-        let loc = WindowToScreen(canvasRef.current, e.clientX, e.clientY);
-        console.log(loc.x.toFixed(0), loc.y.toFixed(0));
-        // let context = canvasRef.current.getContext('2d');
-        let x = loc.x.toFixed(0) - 2;
-        let y = loc.y.toFixed(0) - 2;
-        // context.fillRect(x - x % 10, y - y % 10, 10, 10);
-        let newBoard = DeepCopy(board);
-        // console.log(newBoard);
-        newBoard[(x/10).toFixed(0)][(y/10).toFixed(0)] = 1;
-        setBoard(newBoard);
-    }
+      e.preventDefault();
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        drawGrid(context, 'lightgray', 10, 10);
-        drawBoard(context, board);
-    }, [onMouseClick])
+      let loc = WindowToScreen(canvasRef.current, e.clientX, e.clientY);
+
+      console.log(parseInt(loc.x / stepx), parseInt(loc.y / stepy));
+
+      let x_cord = parseInt(loc.x / stepx);
+      let y_cord = parseInt(loc.y / stepy);
+      
+      boardRef.current[x_cord][y_cord] = 1;
+      boardRef.current[x_cord][y_cord + 1] = 1;
+      boardRef.current[x_cord][y_cord - 1] = 1;
+  }
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
-        setBoard(InitiateBoard(72, 54));
+        const height = canvas.height;
+        const width = canvas.width;
 
-        drawGrid(context, 'lightgray', 10, 10);
+        // boardRef.current = InitiateBoard(parseInt(width / stepx),parseInt(height / stepy));
 
-        drawBoard(context, board);
+        boardRef.current = RandomBoard(parseInt(width / stepx),parseInt(height / stepy));
+
+        drawGrid(context, 'lightgray', stepx, stepy);
+
+        drawBoard(context, boardRef, stepx, stepy);
 
         // const timer = setInterval(() => {
-        //     gameOfLife(board, m_p, n_p);
-        //     drawBoard(context, board, m_p, n_p);
+        //     drawGrid(context, 'lightgray', stepx, stepy);
+        //     drawBoard(context, boardRef, stepx, stepy);
+        //     GameofLife(boardRef);
+        //     // animationFrameId = window.requestAnimationFrame(render);
         // }, 500)
 
         // return () => {
         //     clearInterval(timer);
         // }
 
-        const gameOfLife = () => {
-            if(board.length !== 0){
-                let row = board.length;
-                let column = board[0].length;
-                let v1 = [1, 1, 0, -1, -1, -1, 0, 1];
-                let v2 = [0, 1, 1, 1, 0, -1, -1, -1];
-                let newBoard = DeepCopy(board);
-                for(let i = 0; i < row; i++)
-                {
-                    for(let j = 0; j < column; j++)
-                    {
-                        let liveCell = 0;
-                        for(let x = 0; x < v1.length; x++)
-                        {
-                            let newXCord = i + v1[x];
-                            let newYCord = j + v2[x];
-        
-                            if(newXCord >= 0 && newYCord >= 0 && newXCord < row && newYCord < column)
-                            {
-                                if(newBoard[newXCord][newYCord] == 1)
-                                    liveCell++;
-                            }
-                        }
-                        if(newBoard[i][j] == 0 && liveCell == 3)
-                            board[i][j] = 1;
-                        else if(newBoard[i][j] == 1 && liveCell < 2 || liveCell > 3)
-                            board[i][j] = 0;
-                    }
-                }
-                setBoard(board);
-            }
-        }
-
+        // let frameCount = 0;
         let animationFrameId;
 
         const render = () => {
-            gameOfLife();
+            // draw(context, frameCount);
+            GameofLife(boardRef);
+            drawGrid(context, 'lightgray', stepx, stepy);
+            drawBoard(context, boardRef, stepx, stepy);
             animationFrameId = window.requestAnimationFrame(render);
         }
         render();
@@ -132,10 +114,10 @@ function ConwayLifeGameCanvas()
 
     return (
         <div style={{justifyContent: "center"}}>
-            <div style={{height: "36px"}}>
-                <p> TEST </p>
+            <div style={{height: "32px"}}>
+                <p> CSS IS GOOD </p>
             </div>
-            <canvas ref={canvasRef} onClick={onMouseClick} width="721px" height="541px">
+            <canvas ref={canvasRef} onClick={onMouseClick} width="1030px" height="575px">
 
             </canvas>
         </div>
--- src/ConwayLifeGameCanvas/utils.js
+++ src/ConwayLifeGameCanvas/utils.js
@@ -34,3 +34,51 @@ export function InitiateBoard(row, column)
   }
   return board;
 }
+
+export function RandomBoard(row, column)
+{
+  let board = [];
+  for(let i = 0; i < row; i++)
+  {
+    let temp = [];
+    for(let j = 0; j < column; j++)
+    {
+      Math.random() > 0.3 ? temp.push(0) : temp.push(1);
+    }
+    board.push(temp);
+  }
+  return board;
+}
+
+export function GameofLife(boardRef) {
+  let row = boardRef.current.length;
+  let col = boardRef.current[0].length;
+
+  let v1 = [1, 1, 0, -1, -1, -1, 0, 1];
+  let v2 = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  let temp = DeepCopy(boardRef.current);
+
+  for (let i = 0; i < row; i++)
+  {
+    for (let j = 0; j < col; j++)
+    {
+      let liveCell = 0;
+      for (let x = 0; x < v1.length; x++)
+      {
+        let newXCord = i + v1[x];
+        let newYCord = j + v2[x];
+
+        if (newXCord >= 0 && newYCord >= 0 && newXCord < row && newYCord < col) {
+          if (temp[newXCord][newYCord] == 1)
+            liveCell++;
+        }
+      }
+      if (temp[i][j] == 0 && liveCell == 3)
+        boardRef.current[i][j] = 1;
+      else if (temp[i][j] == 1 && liveCell < 2 || liveCell > 3)
+        boardRef.current[i][j] = 0;
+    }
+  }
+  return;
+}
